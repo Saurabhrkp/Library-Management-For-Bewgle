414c6c50074e383f9fb49866d7b42248cfb9d6ef
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-10 00:57:52 +0530

    Added all books related controllers

---
+++ controllers/index.js
@@ -0,0 +1,89 @@
+const Book = require('../models/book');
+const User = require('../models/user');
+
+exports.getBookByID = async (req, res, next, id) => {
+  try {
+    req.book = await Book.findById(id);
+    if (req.book !== null) {
+      return next();
+    }
+    req.flash(
+      'error_msg',
+      `No Book with id: ${id}, may be because ID is incorrect or modified`
+    );
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.searchBook = async (req, res) => {
+  books = await Book.find({ title: { $regex: req.body.text, $options: 'i' } });
+  if (books.length > 0) {
+    return res.render('index', {
+      PAGE_TITLE: `Result for: ${req.body.text}`,
+      PAGE_PATH: 'index',
+      books,
+    });
+  }
+  req.flash('error_msg', `No Book with name: ${req.body.text}.`);
+  res.redirect('/');
+};
+
+exports.sendBook = (req, res) => {
+  res.render('book', {
+    PAGE_PATH: 'index',
+    PAGE_TITLE: req.book.title,
+    book: req.book,
+    user: req.user,
+  });
+};
+
+exports.getBooks = async (req, res) => {
+  const books = await Book.find({});
+  res.render('index', {
+    PAGE_TITLE: 'All Books',
+    PAGE_PATH: 'index',
+    books,
+  });
+};
+
+exports.getBorrowed = async (req, res) => {
+  const user = await User.findById(req.user._id).populate({
+    path: 'borrowed.books.bookId',
+    select: '-password -address',
+  });
+  const books = user.borrowed.books;
+  res.render('borrow', {
+    PAGE_PATH: 'borrow',
+    PAGE_TITLE: 'Your Borrowed List',
+    books: books,
+    profile: req.profile,
+  });
+};
+
+exports.postBorrowed = async (req, res) => {
+  const bookId = req.body.bookId;
+  await Book.findByIdAndUpdate(bookId, {
+    available: false,
+    $push: { usersBorrowed: req.user.id },
+  });
+  req.user.borrowBook(bookId);
+  res.redirect(`/borrow/${req.user.id}`);
+};
+
+exports.postReturnBorrowedBook = async (req, res) => {
+  const bookId = req.body.bookId;
+  await Book.findByIdAndUpdate(bookId, { available: true });
+  await req.user.returnBorrowedBook(bookId);
+  res.redirect(`/borrow/${req.user.id}`);
+};
+
+exports.postReturnBothBorrowedBook = async (req, res) => {
+  const books = req.body.books;
+  async.each(books, (bookId, callback) => {
+    Book.findByIdAndUpdate(bookId, { available: true }).exec(callback);
+  });
+  await req.user.returnBothBorrowedBook(bookId);
+  res.redirect(`/borrow/${req.user.id}`);
+};
