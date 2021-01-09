e30474f988366e89aacb005e5bee93c70feeebeb
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-09 21:57:46 +0530

    User model with borrow, return and return both methods

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  address: { flat: String, street: String, pincode: String, state: String },
+  username: { type: String, required: true, unique: true },
+  borrowed: {
+    books: [
+      {
+        bookId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Book',
+          required: true,
+        },
+        dueDate: { type: Date, required: true },
+      },
+    ],
+  },
+});
+
+userSchema.methods.borrowBook = function (book) {
+  const borrowedBookIndex = this.borrowed.books.findIndex((cp) => {
+    return cp.bookId.toString() === book._id.toString();
+  });
+  let date = new Date(); // Now
+  date.setDate(date.getDate() + 30); // Set now + 30 days as the new date
+  const updatedBorrowedBooks = [...this.borrowed.books];
+
+  if (borrowedBookIndex >= 0) {
+    updatedBorrowedBooks[borrowedBookIndex].dueDate = date;
+  } else {
+    updatedBorrowedBooks.push({
+      bookId: book._id,
+      dueDate: date,
+    });
+  }
+  const updatedBorrowed = {
+    books: updatedBorrowedBooks,
+  };
+  this.borrowed = updatedBorrowed;
+  return this.save();
+};
+
+userSchema.methods.returnBorrowedBook = function (bookId) {
+  const updatedBorrowedBooks = this.borrowed.books.filter((book) => {
+    return book.bookId.toString() !== bookId.toString();
+  });
+  this.borrowed.books = updatedBorrowedBooks;
+  return this.save();
+};
+
+userSchema.methods.returnBothBorrowedBook = function () {
+  this.borrowed = { books: [] };
+  return this.save();
+};
+
+module.exports = mongoose.model('User', userSchema);
