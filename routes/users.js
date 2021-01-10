c2a29363d5b4c9f0b88d77a644eb5b170ad89980
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-10 15:14:26 +0530

    Added all user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/user');
+const { catchErrors } = require('../controllers/helpers');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+// Register
+router
+  .route('/signup')
+  .get(userController.get_signup)
+  .post(userController.validateSignup, catchErrors(userController.signup));
+
+// Login
+router
+  .route('/signin')
+  .get(userController.get_signin)
+  .post(userController.signin);
+
+// Logout
+router.get('/signout', userController.signout);
+
+router.param('id', userController.getUserByID);
+
+router
+  .route('/:id')
+  .get(userController.checkAuth, userController.getAuthUser)
+  .put(userController.checkAuth, catchErrors(userController.updateUser))
+  .delete(userController.checkAuth, catchErrors(userController.deleteUser));
 
 module.exports = router;
