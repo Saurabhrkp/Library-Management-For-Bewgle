72737fdb665c5d0d994b63da53888dcdc8bd0031
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-10 15:14:08 +0530

    Working on index routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const indexController = require('../controllers/index');
+const userController = require('../controllers/user');
+const { catchErrors, sendFiles } = require('../controllers/helpers');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', catchErrors(indexController.getBooks));
+
+router.param('id', userController.getUserByID);
+
+router
+  .route('/borrow/:id')
+  .get(userController.checkAuth, catchErrors(indexController.getBorrowed))
+  .post(userController.checkAuth, catchErrors(indexController.postBorrowed));
+
+router.post(
+  '/borrow-delete-item',
+  userController.checkAuth,
+  catchErrors(indexController.postReturnBorrowedBook)
+);
+
+router.get('/files/:id', sendFiles);
+
+router.param('id', indexController.getBookByID);
+
+router.post(
+  '/search',
+  userController.checkAuth,
+  catchErrors(indexController.searchBook),
+  indexController.sendBookDetails
+);
+
+router
+  .route('/:id')
+  .get(userController.checkAuth, indexController.sendBookDetails);
 
 module.exports = router;
