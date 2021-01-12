2666007315a7016dea757016a0b06584f54f9c6f
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-12 19:13:08 +0530

    added js file to display cover image on form

---
+++ public/javascripts/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  $('#profileForm :input').prop('disabled', true);
+  $('#editProfile').prop('disabled', false);
+  $('#deletebutton').prop('disabled', false);
+  $('#submitProfile').hide();
+  $('#editProfile').click(function () {
+    if (this.value == 'Edit Profile') {
+      this.value = 'Cancel';
+      $('#profileForm :input').prop('disabled', false);
+      $('#editProfile').prop('disabled', false);
+      $('#deletebutton').prop('disabled', false);
+      $('#submitProfile').show();
+    } else {
+      this.value = 'Edit Profile';
+      $('#profileForm :input').prop('disabled', true);
+      $('#editProfile').prop('disabled', false);
+      $('#deletebutton').prop('disabled', false);
+      $('#submitProfile').hide();
+    }
+  });
+});
+
+$('#coverImageID').change(function (e) {
+  if (e.target.files.length) {
+    if (typeof FileReader != 'undefined') {
+      let coverImageIDPreview = $('#coverImageIDPreview');
+      coverImageIDPreview.html('');
+      $(this).next('.custom-file-label').html(e.target.files[0].name);
+      let reader = new FileReader();
+      let file = e.target.files[0];
+      reader.onload = function (e) {
+        let col = $("<div class='col-sm-5 col-md-4 col-lg-3 mb-3'></div>");
+        let img = $("<img class='d-block w-100'/>");
+        img.attr('src', e.target.result);
+        col.append(img);
+        coverImageIDPreview.append(col);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      alert('This browser does not support HTML5 FileReader.');
+    }
+  } else {
+    $(this).next('.custom-file-label').html('Choose Cover Image');
+  }
+});
