2c0ddd27d310f1a1cada175a7a5ea20decfb9e7b
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2021-01-15 11:13:56 +0530

    Removed list of users who borrowed book

--- models/book.js
+++ models/book.js
@@ -1,18 +1,12 @@
 const mongoose = require('mongoose');
-const shortId = require('crypto-random-string');
 const Schema = mongoose.Schema;
-
-function URI() {
-  return (random = shortId(12));
-}
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   author: { type: String, required: true },
   summary: { type: String, required: true },
-  isbn: { type: String, default: URI() },
-  usersBorrowed: [{ type: Schema.ObjectId, ref: 'User' }],
+  isbn: { type: String, required: true },
   available: { type: Boolean, default: true },
   coverImage: { type: String, required: true },
   coverImageID: { type: String, required: true },
